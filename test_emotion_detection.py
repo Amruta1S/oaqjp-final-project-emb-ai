4c468eff8cf40e211ba08d0748bbe1bf774cf884
Amruta1S <Amruta1.saudagar@ril.com>
2026-03-09 12:52:46 -0400

    Initial commit

---
+++ test_emotion_detection.py
@@ -0,0 +1,14 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+class TestEmotionDetector(unittest.TestCase):
+
+    def test_joy(self):
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result['dominant_emotion'], 'joy')
+
+    def test_anger(self):
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result['dominant_emotion'], 'anger')
+
+unittest.main()
